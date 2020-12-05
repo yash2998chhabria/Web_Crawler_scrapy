31ced5657432b8178afe6f9313fa47476b3c2593
Yash Chhabria <yash2998chhabria@gmail.com>
2020-12-05 20:02:25 +0530

    Initial commit

---
+++ depressionstories/depressionstories/spiders/depressiondataset.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import logging 
+
+class DepressiondatasetSpider(scrapy.Spider):
+    name = 'depressiondataset'
+    allowed_domains = ['www.time-to-change.org.uk']
+    start_urls = ['https://www.time-to-change.org.uk/category/blog/depression/']
+
+    def parse(self, response):
+        stories = response.xpath('//article[@role="article"]')
+        for story in stories:
+            link = story.xpath('.//a[@rel="bookmark"]/@href').get()
+
+            if link is not None:
+                present_link = link
+                yield response.follow(url=present_link, callback=self.parse_story)
+
+    def parse_story(seld,response):
+        name_of_scrapees = response.xpath('//span[@class="blog-name"]/text()').get()
+        blocksofstory = response.xpath('//div[@class="node-content"]//p[text() !="Too many people are made to feel ashamed. By sharing your story, you can help spread knowledge and perspective about mental illness that could change the way people think about it."]/text()').getall()
+
+        yield {
+            'name': name_of_scrapees,
+            'story': blocksofstory
+        }
